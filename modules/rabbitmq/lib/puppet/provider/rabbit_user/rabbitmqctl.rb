9bb62daef0ec9adc87679738d492ac0c62a7cf29
Richard Maynard <richard.maynard@gmail.com>
2012-10-02 12:52:48 -0500

    more fixes, maybe?

--- modules/rabbitmq/lib/puppet/provider/rabbit_user/rabbitmqctl.rb
+++ modules/rabbitmq/lib/puppet/provider/rabbit_user/rabbitmqctl.rb
@@ -6,24 +6,24 @@ Puppet::Type.type(:rabbit_user).provide(:rabbitmqctl) do
   def create
     self.class.send_log(:debug, "call create #{@resource[:name]}")
     %x[ rabbitmqctl add_user #{@resource[:name]} #{@resource[:password]} ]
-  end 
+  end
 
   def destroy
     self.class.send_log(:debug, "call destroy #{@resource[:name]}")
     %x[ rabbitmqctl delete_user #{@resource[:name]} ]
-  end 
+  end
 
   def exists?
     self.class.send_log(:debug, "call exists? #{@resource[:name]}")
-    output = %x[ rabbitmqctl list_users | grep #{resource[:name]}" ]
+    output = %x[ rabbitmqctl list_users | grep #{resource[:name]} ]
     if output =~ /@resource[:name]/ then
       self.class.send_log(:debug, "#{@resource[:name]} exists")
       return true
-    else 
+    else
       self.class.send_log(:debug, "#{@resource[:name]} does not exist output: #{output}")
       return false
     end
 
-  end 
-end 
+  end
+end
 
