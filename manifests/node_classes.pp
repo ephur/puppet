c4e113cb7065df8472f9697cea2a026944722e14
Richard Maynard <richard.maynard@gmail.com>
2012-10-22 16:44:41 -0500

    <3 SABNZBD

--- manifests/node_classes.pp
+++ manifests/node_classes.pp
@@ -1,16 +1,38 @@
 class base(){
+  # Common / Shared components don't reorder these
   include stdlib
+  include puppet-inittab
+
+  # Only other include that should ever be in base is common
+  # if it's another module include it from common so all
+  # includes are in one place
   include common
 }
 
-class media_server(){ 
+class media_server(){
   include base
-  include sabnzbd
+
+  class { "sabnzbd":
+    apikey => $sabnzbd_apikey,
+    nzbkey => $sabnzbd_nzbkey,
+    webuser => $web_user,
+    webpass => $web_pass,
+    nntp_hostname => $sabnzbd_nttp_hostname,
+    nntp_user => $sabnzbd_nttp_user,
+    nntp_pass => $sabnzbd_nttp_pass,
+    nntp_ssl => $sabnzbd_nttp_ssl,
+    sickbeard_hostname => "localhost",
+
+  }
+
   include couchpotato
+
   include sickbeard
+
   include headphones
 }
 
 class rabbit(){
+  include base
   include rabbitmq
 }
--- modules/sabnzbd/manifests/init.pp
+++ modules/sabnzbd/manifests/init.pp
@@ -1,7 +1,69 @@
-class sabnzbd(){ 
+class sabnzbd($apikey,$webuser,$webpass,$nntp_hostname,$nntp_user,$nntp_pass,$nzbkey="",$nntp_port=119,
+              $nntp_ssl=0,$nntp_connections=10,$nntp_retention=0,$sickbeard_hostname=nil,$sickbeard_ssl=1,
+              $sickbeard_port=8081,$sickbeard_user=nil,$sickbeard_pass=nil,$nzbkey=nil,
+              $bwlimit="none",$base_dir='/etc/sabnzbd',$user=root,$group=nil,$nzb_upload_dir=""){
+
+  if $nntp_port == 119 and $nntp_ssl == 1 {
+    $nntp_port = 563
+  }
+
+  if $group == nil {
+    $group = $user
+  }
+
+  if $base_dir == '/etc/sabnzbd'{
+    file { $base_dir:
+      ensure => directory,
+      owner => $user,
+      group => $user,
+      mode => 0640
+    }
+  }
+
   vcsrepo { '/usr/local/apps/sabnzbd':
-    ensure   => present,
+    ensure   => latest,
     provider => git,
-    source   => 'git://github.com/sabnzbd/sabnzbd.git'
+    source   => 'git://github.com/sabnzbd/sabnzbd.git',
+    owner => $user,
+    group => $group,
+    user => $user,
+    require => [User[$user]];
+  }
+
+  file { "${base_dir}/sabnzbd.ini":
+    ensure => present,
+    owner => $user,
+    group => $group,
+    mode => 0640,
+    content => template("sabnzbd/sabnzbd.ini.erb");
+
+    '${base_dir}/post-process-scripts':
+      ensure => directory,
+      recurse => true,
+      owner => $user,
+      group => $group,
+      source => "puppet:///modules/sabnzbd/post-process-scripts",
+      mode => 0755;
+   }
+
+  if $sickbeard_hostname != nil {
+    if $sickbeard_pass == nil or $sickbeard_user ==nil {
+      err("password or username not set, disable sickbeard integration or update pass sickbeard_pass and sickbeard_user to the module")
+    } else {
+      file {
+        "${base_dir}/sab_nzb_to_sickbeard.ini":
+          ensure => present,
+          owner => $user,
+          group => $group,
+          mode => 0640,
+          content => template("sabnzbd/sab_nzb_to_sickbeard.ini.erb");
+
+        '${base_dir}/post-process-scripts/autoProcessTV.cfg':
+          ensure => symlink,
+          owner => $user,
+          group => $group,
+          source => '${base_dir}/sab_nzb_to_sickbeard.ini';
+      }
+    }
   }
 }
