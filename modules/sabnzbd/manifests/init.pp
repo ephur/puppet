814f79a2d037089b39e84c6e8bec5b992bd82117
Richard Maynard <richard.maynard@gmail.com>
2012-10-22 17:56:05 -0500

    debugging

--- modules/sabnzbd/manifests/init.pp
+++ modules/sabnzbd/manifests/init.pp
@@ -88,6 +88,7 @@ class sabnzbd($apikey,$webuser,$webpass,$nntp_hostname,$nntp_user,$nntp_pass,$nz
           owner => $user,
           group => $use_group,
           source => '/${base_dir}/sab_nzb_to_sickbeard.ini';
+          require => [File['sab_nzb_to_sickbeard.ini']]
       }
     }
   }
