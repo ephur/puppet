4374ada7b48a3c63e7ab9dd1b678582e917c19fa
Richard Maynard <richard.maynard@gmail.com>
2012-10-23 05:30:57 -0500

    adding more modules

---
+++ modules/ajaxplorer/manifests/init.pp
@@ -0,0 +1,27 @@
+class ajaxplorer(){
+
+  file { "/etc/apt/sources.list.d/ajaxplorer.list":
+    ensure => present,
+    owner => "root",
+    group => "root",
+    mode => 644,
+    content => "deb http://dl.ajaxplorer.info/repos/apt ${lsbdistcodename} main",
+    require => Exec["ajaxplorer-apt-key"]
+  }
+
+  exec { "ajaxplorer-apt-key":
+    command => "/usr/bin/wget -O- http://dl.ajaxplorer.info/repos/charles@ajaxplorer.info.gpg.key | /usr/bin/apt-key add -",
+    unless => "/usr/bin/apt-key list | /bin/grep 4096R/4BD6EC30",
+    path => ["/bin", "/usr/bin"],
+    timeout => "10",
+  }
+
+
+  exec { "ajaxplorer-apt-update":
+    command => "/usr/bin/apt-get update",
+    path => ["/bin", "/usr/bin"],
+    subscribe => [Exec[ajaxplorer-apt-key],File["/etc/apt/sources.list.d/ajaxplorer.list"]],
+    refreshonly => true
+  }
+
+}
