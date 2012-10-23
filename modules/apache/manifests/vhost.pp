95f79de6d3f912f2dd42f03db0a8aa825e9c3b45
Richard Maynard <richard.maynard@gmail.com>
2012-10-23 11:34:35 -0500

    le sigh

--- modules/apache/manifests/vhost.pp
+++ modules/apache/manifests/vhost.pp
@@ -2,10 +2,16 @@ define apache::vhost($priority=10, $ensure = 'present') {
 
   include apache
 
+  if $ensure == 'present'{
+    $content = template("apache/vhosts/${name}")
+  } else {
+    $content = ""
+  }
+
   file { "${priority}-${name}.conf":
     ensure  => $ensure,
     path    => "/etc/apache2/sites-enabled/${priority}-${name}.conf",
-    content => template("apache/vhosts/${name}"),
+    content => $content,
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
