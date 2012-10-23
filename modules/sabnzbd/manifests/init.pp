a6e620b3cff3ed658981d0d18076b36b548590c5
Richard Maynard <richard.maynard@gmail.com>
2012-10-23 12:43:37 -0500

    debugging & deploying

--- modules/sabnzbd/manifests/init.pp
+++ modules/sabnzbd/manifests/init.pp
@@ -1,7 +1,7 @@
 class sabnzbd($apikey,$webuser,$listen_port=8080,$webpass,$nntp_hostname,$nntp_user,$nntp_pass,$nzbkey="",$nntp_port=119,
               $nntp_ssl=0,$nntp_connections=10,$nntp_retention=0,$sickbeard_hostname=nil,$sickbeard_ssl=1,
               $sickbeard_port=8081,$sickbeard_user=nil,$sickbeard_pass=nil,$nzbkey=nil, $listen_ip='0.0.0.0',
-              $bwlimit="none",$base_dir='/etc/sabnzbd',$user=root,$group=nil,$nzb_upload_dir="",
+              $bwlimit="none",$base_dir='/etc/sabnzbd',$user=root,$group=nil,$nzb_upload_dir="",$use_ssl=0,
               $use_couchpotato = 0, $use_headphones=1, $headphones_download_path=nil, $nzbmatrix_username=nil, $nzbmatrix_password=nil,
               $path='usr/local/apps/sabnzbd',$download_path="Downloads",$log_path="/dev/null",$user_groups=[]){
 
