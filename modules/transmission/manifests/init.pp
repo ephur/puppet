0b94d5c71201df9b2de5368d5f0a7082a01a12dd
Richard Maynard <richard.maynard@gmail.com>
2012-10-29 11:56:25 -0500

    debugging

--- modules/transmission/manifests/init.pp
+++ modules/transmission/manifests/init.pp
@@ -1,31 +1,31 @@
-class transmission($download_dir="/mnt/data/Downloads/torrent-incomplete",){ 
+class transmission($download_dir="/mnt/data/Downloads/torrent-incomplete",){
 
-  package { ["transmission","transmission-daemon","transmission-cli"]: 
+  package { ["transmission","transmission-daemon","transmission-cli"]:
     ensure =>  latest
-  } 
+  }
 
   service { "transmission-daemon":
     ensure  => running,
-    require => Package["transmission-daemon"],File["/etc/transmission-daemon/settings.json"]
-  } 
- 
-  file { 
+    require => [Package["transmission-daemon"],File["/etc/transmission-daemon/settings.json"]]
+  }
+
+  file {
 
     "/etc/transmission-daemon/settings.json":
-      ensure  => present, 
-      owner   => debian-transmission, 
+      ensure  => present,
+      owner   => debian-transmission,
       group   => mediaserver,
-      mode    => 0644, 
+      mode    => 0644,
       content => template("transmission/settings.json.erb"),
-      require => Package["transmission-daemon"]; 
- 
+      require => Package["transmission-daemon"];
+
     ["/mnt/data/Downloads/torrent-incomplete", "/mnt/data/Downloads/torrent-complete", "/mnt/data/Downloads/torrents"]:
       ensure  => directory,
       owner   => debian-transmission,
       group   => mediaserver,
-      mode    => 0755, 
+      mode    => 0755,
       require => Package["transmission-daemon"]
   }
 
 }
- 
+
