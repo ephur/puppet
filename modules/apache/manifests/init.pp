e36c9974591b75717ca05f40e40a94b8b84e801c
Richard Maynard <richard.maynard@gmail.com>
2013-01-28 17:41:35 -0600

    PuppetLabs module changes
    * added apache module
    * updated STDLIB module
    * updated FIREWALL module (to support apache module)

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,25 +1,72 @@
-class apache(){
+# Class: apache
+#
+# This class installs Apache
+#
+# Parameters:
+#
+# Actions:
+#   - Install Apache
+#   - Manage Apache service
+#
+# Requires:
+#
+# Sample Usage:
+#
+class apache (
+  $default_mods = true,
+  $service_enable = true,
+  $serveradmin  = 'root@localhost',
+  $sendfile     = false
+) {
+  include apache::params
 
-  package { ['apache2']:
-    ensure => latest;
+  package { 'httpd':
+    ensure => installed,
+    name   => $apache::params::apache_name,
   }
 
-  service { 'apache2':
-    ensure    => running,
-    enable    => true,
-    subscribe => Package['apache2'];
+  # true/false is sufficient for both ensure and enable
+  validate_bool($service_enable)
+
+  service { 'httpd':
+    ensure    => $service_enable,
+    name      => $apache::params::apache_name,
+    enable    => $service_enable,
+    subscribe => Package['httpd'],
   }
 
-  file { '/etc/apache2/ssl':
-    ensure => directory,
-    owner  => root,
-    group  => root,
-    mode   => '0640';
+  file { 'httpd_vdir':
+    ensure  => directory,
+    path    => $apache::params::vdir,
+    recurse => true,
+    purge   => true,
+    notify  => Service['httpd'],
+    require => Package['httpd'],
   }
 
-  a2mod { ['proxy','ssl','rewrite','proxy_http']:
-    ensure => present,
-    notify => Service['apache2'];
+  if $apache::params::conf_dir and $apache::params::conf_file {
+    # Template uses:
+    # - $apache::params::user
+    # - $apache::params::group
+    # - $apache::params::conf_dir
+    # - $serveradmin
+    file { "${apache::params::conf_dir}/${apache::params::conf_file}":
+      ensure  => present,
+      content => template("apache/${apache::params::conf_file}.erb"),
+      notify  => Service['httpd'],
+      require => Package['httpd'],
+    }
+    if $default_mods == true {
+      include apache::mod::default
+    }
   }
-
+  if $apache::params::mod_dir {
+    file { $apache::params::mod_dir:
+      ensure  => directory,
+      require => Package['httpd'],
+    } -> A2mod <| |>
+    resources { 'a2mod':
+      purge => true,
+    }
+  }
 }
